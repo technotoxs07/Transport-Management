27fe5e4c6e0fbbd7c1260972f5b2703faeb248b1
ronitshrivas <ronit.shrivastav03@gmail.com>
2023-12-14 16:19:07 +0545

    Registration update

---
+++ CurierManagementSystemCSharp/RegistrationForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace CurierManagementSystemCSharp
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Key";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(311, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 100);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(311, 33);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(351, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(436, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // RegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 201);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RegistrationForm";
+            this.Text = "RegistrationForm";
+            this.Load += new System.EventHandler(this.RegistrationForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ CurierManagementSystemCSharp/RegistrationForm.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurierManagementSystemCSharp
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+        public string ProductID { get; private set; }
+        public string ProductKey { get; private set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Please enter all required information.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+           
+
+            // Check if the experience days have passed since registration
+         
+
+            // Continue with the registration logic
+            ProductID = textBox1.Text;
+            ProductKey = textBox2.Text;
+            if (!IsLicenseKeyValid(ProductKey))
+            {
+                MessageBox.Show("Invalid license key. Please enter a valid key.", "Invalid Key", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (IsLicenseAlreadyRegistered(ProductKey))
+            {
+                // Extract the number of experience days from the product key
+                int experienceDays = ExtractExperienceDaysFromProductKey(ProductKey);
+
+                // Check if the experience days have passed since registration
+                if (HasExperienceDaysPassed(experienceDays))
+                {
+                    MessageBox.Show("Experience days have passed since registration.", "Expired", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    // Open the registration form for re-registration
+                    OpenRegistrationForm();
+                    return;
+                }
+
+                // Open the Login form directly
+               
+                return;
+            }
+            // Optionally, you can perform additional validation or processing here
+            if (IsLicenseAlreadyRegistered(textBox2.Text))
+            {
+                MessageBox.Show("This license is already registered.", "Duplicate Registration", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Login lg = new Login();
+                lg.ShowDialog();
+                this.Close();
+                return;
+            }
+
+            // Continue with the registration logic
+            RegisterLicense(textBox2.Text);
+            // Close the registration form with DialogResult.OK to indicate success
+            DialogResult = DialogResult.OK;
+            MessageBox.Show("Successfully Registered", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Login gl = new Login();
+            gl.ShowDialog();
+            this.Close();
+
+        }
+
+        private void OpenRegistrationForm()
+        {
+            RegistrationForm rf = new RegistrationForm();
+            rf.ShowDialog();
+
+        }
+
+        private bool IsLicenseKeyValid(string productKey)
+        {
+            productKey = productKey.Trim();
+
+            // Check length and character set
+            if (productKey.Length != 25 || !productKey.All(char.IsLetterOrDigit))
+            {
+                MessageBox.Show("Invalid product key.", "Invalid Key", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            // Additional validation logic can be added based on your specific requirements
+
+            return true;
+        }
+
+        private void RegisterLicense(string licenseKey)
+        {
+            string filePath = Path.Combine(Application.StartupPath, "RegisteredLicenses.txt");
+
+            // Check if the file exists; if not, create the file
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    // Create the file
+                    File.Create(filePath).Close();
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show($"Error creating the file: {ex.Message}", "File Creation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            // Append the new license key to the file
+            try
+            {
+                File.AppendAllLines(filePath, new[] { licenseKey });
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"Error writing to the file: {ex.Message}", "File Write Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private bool IsLicenseAlreadyRegistered(string licenseKey)
+        {
+            string filePath = Path.Combine(Application.StartupPath, "RegisteredLicenses.txt");
+
+            // Check if the license key exists in the file
+            return System.IO.File.Exists(filePath) && System.IO.File.ReadAllLines(filePath).Contains(licenseKey);
+            
+        }
+
+        private bool HasExperienceDaysPassed(int experienceDays)
+        {
+            // Retrieve the date of registration (you need to store this information during registration)
+            DateTime registrationDate = DateTime.Now; // Replace with the actual registration date
+
+            // Calculate the expiration date
+            DateTime expirationDate = registrationDate.AddDays(experienceDays);
+
+            // Check if the current date is after the expiration date
+            return DateTime.Now > expirationDate;
+        }
+
+        private int ExtractExperienceDaysFromProductKey(string productKey)
+        {
+            string[] parts = productKey.Split('-');
+
+            // Retrieve the encoded experience days (assuming it's the third part)
+            if (parts.Length >= 3)
+            {
+                string encodedExperienceDays = parts[2];
+
+                // Decode the experience days from the encoded string
+                if (int.TryParse(encodedExperienceDays, out int experienceDays))
+                {
+                    return experienceDays;
+                }
+            }
+
+            // Return a default value or handle the error as needed
+            return 0;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RegistrationForm_Load(object sender, EventArgs e)
+        {
+            if (IsLicenseAlreadyRegistered(textBox2.Text))
+            {
+              
+                Login lg = new Login();
+                lg.ShowDialog();
+                this.Close();
+                return;
+            }
+        }
+    }
+}
