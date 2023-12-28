74d24f35201d0e0c9b7592fa2d90703a72951871
ronitshrivas <ronit.shrivastav03@gmail.com>
2023-12-28 21:28:41 +0545

    new update second

--- CurierManagementSystemCSharp/StaffDetails.Designer.cs
+++ CurierManagementSystemCSharp/StaffDetails.Designer.cs
@@ -442,7 +442,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Name = "StaffDetails";
+            
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StaffDetails";
             this.Load += new System.EventHandler(this.StaffDetails_Load);
